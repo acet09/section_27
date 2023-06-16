46e1a425f37088929ab31669a5efd0dc4e7f9e74
Cactus <ysjungman@gmail.com>
2023-06-16 11:05:00 +0900

    428_typescript-배열및객체

---
+++ basics.ts
@@ -0,0 +1,34 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+//Primitives
+let age: number;
+age = 1.12;
+
+let userName: string;
+userName = "name";
+
+let isTinstructor: boolean;
+isTinstructor = true;
+
+//More complex types
+let hobbies: string[];
+hobbies = ["1", "2", "3"];
+
+let person: {
+  name: string;
+  age: number;
+};
+person = {
+  name: "Max",
+  age: 32,
+};
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
